172ed8e769f20d591ff646b5a99c7855a32a3660
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-01-05 22:52:08 +0000

    Pin sphinx ,et all version to match deve environmetn.

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,6 @@ html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     "show_toc_level": 2    
 }
-html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
